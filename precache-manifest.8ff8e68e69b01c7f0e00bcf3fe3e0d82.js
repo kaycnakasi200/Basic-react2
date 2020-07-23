97009aec0505e4464ca7951e5f4f8a8ba27a6761
besic-web <kaycnakasi200@gmail.com>
2020-07-23 12:38:37 +0700

    Updates

---
+++ precache-manifest.8ff8e68e69b01c7f0e00bcf3fe3e0d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae65ffcf83d9e7f3a404dbeed46a848",
+    "url": "/Basic-react2/index.html"
+  },
+  {
+    "revision": "328d091d351ce9009690",
+    "url": "/Basic-react2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "784ab9efcccbf5e2e100",
+    "url": "/Basic-react2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "328d091d351ce9009690",
+    "url": "/Basic-react2/static/js/2.ea5ef707.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Basic-react2/static/js/2.ea5ef707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784ab9efcccbf5e2e100",
+    "url": "/Basic-react2/static/js/main.992c8b48.chunk.js"
+  },
+  {
+    "revision": "e4ee78d10f7f3a76119e",
+    "url": "/Basic-react2/static/js/runtime-main.702f6654.js"
+  }
+]);
